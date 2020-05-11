f1de100af5c3d7d3255bfddbba90516060de9f95
Tawheed Haroon <tawheed.haroon@gmail.com>
2020-05-11 13:48:42 -0400

    more app js

--- Develop/app.js
+++ Develop/app.js
@@ -15,15 +15,12 @@ const employeeQuestions = [
         type: "input",
         name: "employeeName",
         message: "What is your name?",
-
     },
-
     {
         type: "input",
         name: "employeeId",
         message: "What is your ID?",
     },
-
     {
         type: "input",
         name: "employeeEmail",
@@ -36,27 +33,22 @@ const managerQuestions = [
         type: "input",
         name: "managerName",
         message: "What is your name?",
-
     },
-
     {
         type: "input",
         name: "managerId",
         message: "What is your ID?",
     },
-
     {
         type: "input",
         name: "managerEmail",
         message: "What is your e-mail?",
     },
-
     {
         type: "input",
         name: "managerOfficeNumber",
         message: "What is your office number?",
     }
-
 ];
 
 const teamMembers = [
@@ -67,36 +59,27 @@ const teamMembers = [
     }
 ]
 
-
-
 const engineerQuestions = [
     {
         type: "input",
         name: "engineerName",
         message: "What is your name?"
-
     },
-
     {
         type: "input",
         name: "engineerId",
         message: "What is your ID?"
-
     },
-
     {
         type: "input",
         name: "engineerEmail",
         message: "What is your e-mail?",
     },
-
-
     {
         type: "input",
         name: "engineerGitHub",
         message: "What is your  GitHub name?",
     }
-
 ]
 
 const internQuestions = [
@@ -104,48 +87,39 @@ const internQuestions = [
         type: "input",
         name: "internName",
         message: "What is your name?"
-
     },
-
     {
         type: "input",
         name: "internId",
         message: "What is your ID?"
-
     },
-
     {
         type: "input",
         name: "internEmail",
         message: "What is your e-mail?",
     },
-
-
     {
         type: "input",
         name: "internSchool",
         message: "Where did you go to school?",
     }
-
 ]
 
 function createEngineer() {
     inquirer.prompt(engineerQuestions).then(function (engineerResponse) {
         console.log(engineerResponse);
         const teamEngineer = new Engineer(engineerResponse.engineerName, engineerResponse.engineerId,
-        engineerResponse.engineerEmail, engineerResponse.engineerGitHub);
+            engineerResponse.engineerEmail, engineerResponse.engineerGitHub);
         promptTeam();
         employee.push(teamEngineer)
 
     })
-
-
 }
 
 function createIntern() {
     inquirer.prompt(internQuestions).then(function (internResponse) {
-        const teamIntern = new Intern (internResponse.internName, internResponse.internId,
-        internResponse.internEmail, internResponse.internSchool);
+        const teamIntern = new Intern(internResponse.internName, internResponse.internId,
+            internResponse.internEmail, internResponse.internSchool);
         console.log(internResponse);
         promptTeam();
         employee.push(teamIntern)
@@ -155,14 +129,14 @@ function createIntern() {
 function buildTeam() {
     console.log(employee);
     fs.writeFileSync(outputPath, render(employee));
-    
+
 }
 
 function createManager() {
     inquirer.prompt(managerQuestions).then(managerResponse => {
         console.log(managerResponse);
-        const teamManager = new Manager (managerResponse.managerName, managerResponse.managerId,
-        managerResponse.managerEmail, managerResponse.managerOfficeNumber);
+        const teamManager = new Manager(managerResponse.managerName, managerResponse.managerId,
+            managerResponse.managerEmail, managerResponse.managerOfficeNumber);
         promptTeam();
         employee.push(teamManager);
     })
@@ -181,7 +155,5 @@ function promptTeam() {
         }
     })
 }
-
-
 
 createManager();
